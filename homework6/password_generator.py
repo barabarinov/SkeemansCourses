80da30e286f14a5e42a2e09cf721e37cb95e112a
aleksandrbarinov <barabarinov@gmail.com>
2021-10-02 19:38:55 +0300

    release v3

--- homework6/password_generator.py
+++ homework6/password_generator.py
@@ -1,27 +1,32 @@
 import string
 import random
 
-# digits = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-cymbols1 = '/', '/', ',', '<', '>', ':', '^', '(', ')', '[', ']', '{', '}'
-cymbols2 = '$', '@', '!', '%', '#', '*', '&'
+cymbols1 = '$', '@', '!', '%', '#', '*', '&'
+cymbols2 = '/', '/', ',', '<', '>', ':', '^', '(', ')', '[', ']', '{', '}'
+password_components = ()
 
 print('PASSWORD GENERATOR')
 print('The password generator will help you to create hard-to-crack passwords.\nJust fill out the form below and click the Generate button.')
 
 password_length = int(input('Length of the password: '))
-password_components = string.ascii_lowercase + string.ascii_uppercase + string.digits + cymbols_sum = i for i in range(cymbols1)
-password = ''.join(random.choice(password_components) for i in range(password_length))
-print(password)
+# password_components = string.ascii_lowercase + string.ascii_uppercase + string.digits + '[]','{}', '()' + '@', '%', '$'
 
-# cymbols_input = input('Include symbols like ($@!%#*&)? Yes or no!')
-# if cymbols_input == 'yes'
-# cymbols_input2 = ('Include symbols like (/ \ : () [] {} ^ < >)? Yes or no!')
+
+cymbols_input = input('Include symbols like ($@!%#*&)? Yes or no!').lower()
+if cymbols_input == 'yes':
+    password_components + cymbols1
+else:
+cymbols_input2 = ('Include symbols like (/ \ : () [] {} ^ < >)? Yes or no!')
+
+if cymbols_input == 'yes':
+    password_components + cymbols2
+else:
 # lower_case = input('Include lowercase symbols? Yes or no!')
 # upper_case = input('Include uppercase? Yes or no!')
 # numbers_input = ('Include numbers? Yes or no!')
 
-# password_length = random.choice(cymbols1, cymbols_input2, digits, string.ascii_letters)
-# print(password)
+password = ''.join(random.choice(password_components) for i in range(password_length))
+print(password)
 
 
 # import string
