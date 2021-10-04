feda054948420116cade245631dbf2df66c5ff9b
aleksandrbarinov <barabarinov@gmail.com>
2021-10-04 21:55:54 +0300

    release v5

---
+++ homework6/List_Comprehensions.py
@@ -0,0 +1,7 @@
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+digits = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'e', 'f', 'g']
+my_list = [n+2 for n in nums]
+print(my_list)
+
+new_list = [(digit, num) for digit in (digits) for num in range(4)]
+print(new_list)
--- homework6/password_gen.py
+++ homework6/password_gen.py
@@ -1,3 +1,4 @@
+#СТАРАЯ ВЕРСИЯ
 import string
 import random
 
@@ -16,31 +17,30 @@ def get_password_length():
     return password_length
 
 def get_selected_symbols():
-    password_components_signs = []
-    password_components_letters_digits = []
+    password_components = []
 
     cymbols1 = ['$', '@', '!', '%', '#', '*', '&']
     cymbols2 = ['/', '/', ',', '<', '>', ':', '^', '(', ')', '[', ']', '{', '}']
     cymbols_input = input('Include symbols like ($ @ ! % # * &)? Yes or no! ').lower()
     if cymbols_input == 'yes':
-        password_components_signs += cymbols1
+        password_components += cymbols1
 
     another_cymbols_input2 = input('Include symbols like (/ \ : () [] {} ^ < >)? Yes or no! ').lower()
     if another_cymbols_input2 == 'yes':
-        password_components_signs += cymbols2
+        password_components += cymbols2
 
     lower_case = input('Include lowercase symbols? Yes or no! ').lower()
     if lower_case == 'yes':
-        password_components_letters_digits += string.ascii_lowercase
+        password_components += string.ascii_lowercase
 
     upper_case = input('Include uppercase? Yes or no! ').lower()
     if upper_case == 'yes':
-        password_components_letters_digits += string.ascii_uppercase
+        password_components += string.ascii_uppercase
 
     numbers_input = input('Include numbers? Yes or no! ').lower()
     if numbers_input == 'yes':
-        password_components_letters_digits += string.digits
-    return password_components_signs, password_components_letters_digits
+        password_components += string.digits
+    return password_components
 
 def get_password(selected_symbols, password_length):
     password = ''.join(random.choice(selected_symbols) for i in range(password_length))
@@ -48,8 +48,8 @@ def get_password(selected_symbols, password_length):
 
 def main():
     password_length = get_password_length()
-    selected_symbols, digits_and_letters = get_selected_symbols()
-    print(get_password(selected_symbols + digits_and_letters, password_length))
+    selected_symbols = get_selected_symbols()
+    print(get_password(selected_symbols, password_length))
 
 if __name__ == '__main__':
     main()
