018ee3514b5235e371dc5b66e44c44176c6b9310
aleksandrbarinov <barabarinov@gmail.com>
2021-10-04 11:26:16 +0300

    release v4

--- homework6/password_generator.py
+++ homework6/password_generator.py
@@ -1,39 +1,54 @@
 import string
 import random
 
-cymbols1 = '$', '@', '!', '%', '#', '*', '&'
-cymbols2 = '/', '/', ',', '<', '>', ':', '^', '(', ')', '[', ']', '{', '}'
-password_components = ()
 
 print('PASSWORD GENERATOR')
-print('The password generator will help you to create hard-to-crack passwords.\nJust fill out the form below and click the Generate button.')
+print(
+    'The password generator will help you to create hard-to-crack passwords.\nJust fill out the form below and click the Generate button.')
 
-password_length = int(input('Length of the password: '))
+def get_password_length():
+    while True:
+        try:
+            password_length = int(input('Length of the password: '))
+        except ValueError as e:
+            print(f"ERROR {e}")
+        else:
+            break
+    return password_length
 
-cymbols_input = input('Include symbols like ($@!%#*&)? Yes or no!').lower()
-if cymbols_input == 'yes':
-    password_components = cymbols1
+def get_selected_symbols():
+    password_components = []
 
-cymbols_input2 = ('Include symbols like (/ \ : () [] {} ^ < >)? Yes or no!')
-if cymbols_input2 == 'yes':
-    password_components = cymbols2
+    cymbols1 = ['$', '@', '!', '%', '#', '*', '&']
+    cymbols2 = ['/', '/', ',', '<', '>', ':', '^', '(', ')', '[', ']', '{', '}']
+    cymbols_input = input('Include symbols like ($ @ ! % # * &)? Yes or no! ').lower()
+    if cymbols_input == 'yes':
+        password_components += cymbols1
 
-lower_case = input('Include lowercase symbols? Yes or no!')
-if lower_case == 'yes':
-    password_components = string.ascii_lowercase
+    another_cymbols_input2 = input('Include symbols like (/ \ : () [] {} ^ < >)? Yes or no! ').lower()
+    if another_cymbols_input2 == 'yes':
+        password_components += cymbols2
 
-upper_case = input('Include uppercase? Yes or no!')
-# numbers_input = ('Include numbers? Yes or no!')
+    lower_case = input('Include lowercase symbols? Yes or no! ').lower()
+    if lower_case == 'yes':
+        password_components += string.ascii_lowercase
 
-# password_components = string.ascii_lowercase + string.ascii_uppercase + string.digits + '[]','{}', '()' + '@', '%', '$'
-password = ''.join(random.choice(password_components) for i in range(password_length))
-print(password)
+    upper_case = input('Include uppercase? Yes or no! ').lower()
+    if upper_case == 'yes':
+        password_components += string.ascii_uppercase
 
+    numbers_input = input('Include numbers? Yes or no! ').lower()
+    if numbers_input == 'yes':
+        password_components += string.digits
+    return password_components
 
-# import string
-# string.ascii_uppercase
-# 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-# string.ascii_lowercase
-# 'abcdefghijklmnopqrstuvwxyz'
-# string.ascii_letters
-# 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+def get_password(selected_symbols, password_length):
+    password = ''.join(random.choice(selected_symbols) for i in range(password_length))
+    return (f'Your password is {password}')
+
+def main():
+    return (get_password(get_selected_symbols(), get_password_length()))
+    print(get_password())
+
+if __name__ == '__main__':
+    main()
--- homework6/rock_paper_scissors.py
+++ homework6/rock_paper_scissors.py
@@ -1,10 +1,10 @@
 import random
 
-# rock paper scissors
-game_values = ['ROCK', 'SCISSORS', 'PAPER']
+# ROCK, PAPER & SCISSORS
+game_values = ['rock', 'scissors', 'paper']
 print('ROCK, PAPER & SCISSORS')
 while True:
-    players_choice = input('You can choose: Rock, Paper or Scissors. Make your choice!: ').upper()
+    players_choice = input('You can choose: Rock, Paper or Scissors. Make your choice!: ').lower()
     if players_choice not in game_values:
         print('Wrong value!')
         continue
@@ -17,38 +17,48 @@ while True:
         print('Tie, try again')
         continue
 
-    if players_choice == 'PAPER':
-        if computer_choice == 'ROCK':
+    if players_choice == 'paper':
+        if computer_choice == 'rock':
             print('You won! Congrats')
-        elif computer_choice == 'SCISSORS':
+        elif computer_choice == 'scissors':
             print('You lose! But that\'s okay!')
 
-    elif players_choice == 'ROCK':
-        if computer_choice == 'SCISSORS':
+    elif players_choice == 'rock':
+        if computer_choice == 'scissors':
             print('You won! Congrats')
-        elif computer_choice == 'PAPER':
+        elif computer_choice == 'paper':
             print('You lose! Sorry bro!')
 
-    elif players_choice == 'SCISSORS':
-        if computer_choice == 'PAPER':
+    elif players_choice == 'scissors':
+        if computer_choice == 'paper':
             print('You won! Congrats')
-        elif computer_choice == 'ROCK':
+        elif computer_choice == 'rock':
             print('Oops! You lose! Well, don\'t be discouraged!')
 
-    ask = input('Would you like to play again? ').upper()
-    if ask == 'YES':
+    ask = input('Would you like to play again? ').lower()
+    if ask == 'yes':
         continue
     else:
         print('See ya!')
         break
 
-#ВАРИАНТ КОНЦОВКИ С ЦИКЛОМ
-# ask = input('Would you like to play again? ').upper()
-# while (ask != 'YES') and (ask != 'NO'):
+#ВАРИАНТ КОНЦОВКИ С ЦИКЛОМ1
+# ask = input('Would you like to play again? ').lower()
+# while (ask != 'yes') and (ask != 'no'):
 #     print('Wrong value')
-#     ask = input('Would you like to play again? ').upper()
-# if ask == 'YES':
+#     ask = input('Would you like to play again? ').lower()
+# if ask == 'yes':
 #     continue
-# elif ask == 'NO':
+# elif ask == 'no':
 #     print('See ya!')
+#     break
+
+#ВАРИАНТ КОНЦОВКИ С ЦИКЛОМ2
+# is_exit = False
+# while True:
+#     inp = input('Do you want to play again?').lower()
+#     if inp in ['yes', 'no']:
+#         is_exit = inp == 'no'
+#         break
+# if is_exit:
 #     break
