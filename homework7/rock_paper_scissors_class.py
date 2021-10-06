bae0ad855ab918796457faf44d0c9e6d46c00ac8
aleksandrbarinov <barabarinov@gmail.com>
2021-10-06 16:31:01 +0300

    release v2

--- homework7/rock_paper_scissors_class.py
+++ homework7/rock_paper_scissors_class.py
@@ -1,69 +1,63 @@
-import my_random
+import random
 
-class Rock_Paper_Scissors:
+class RockPaperScissors:
 
-    def __init__(self, game_values,):
-        self.game_values = game_values ['rock', 'scissors', 'paper']
+def choices_of_players():
+    game_values = ['rock', 'scissors', 'paper']
+    while True:
+        players_choice = input('You can choose: Rock, Paper or Scissors. Make your choice!: ').lower()
+        if players_choice not in game_values:
+            print('Wrong value!')
+            continue
+        else:
+            print('\nYOU: ' + players_choice)
+            computers_choice = random.choice(game_values)
+            print(f'COMPUTER: ' + computers_choice)
+        return players_choice, computers_choice
 
-        # print('ROCK, PAPER & SCISSORS')
-    def choices_of_players(self):
-        while True:
-            players_choice = input('You can choose: Rock, Paper or Scissors. Make your choice!: ').lower()
-            if players_choice not in game_values:
-                print('Wrong value!')
-                continue
-            else:
-                print('\nYOU: ' + players_choice)
-                computer_choice = my_random.choice(game_values)
-                print(f'COMPUTER: ' + computer_choice)
+def dead_heat(players_choice, computers_choice):
+    while True:
+        if players_choice == computers_choice:
+            print('Tie, try again')
+            players_choice, computers_choice = choices_of_players()
+        else:
+            return players_choice, computers_choice
 
-            if players_choice == computer_choice:
-                print('Tie, try again')
-                continue
+def game_progress(players_choice, computers_choice):
+    if players_choice == 'paper':
+        if computers_choice == 'rock':
+            return 'You won! Congrats'
+        elif computers_choice == 'scissors':
+            return 'You lose! But that\'s okay!'
 
-            if players_choice == 'paper':
-                if computer_choice == 'rock':
-                    print('You won! Congrats')
-                elif computer_choice == 'scissors':
-                    print('You lose! But that\'s okay!')
+    elif players_choice == 'rock':
+        if computers_choice == 'scissors':
+            return 'You won! Congrats'
+        elif computers_choice == 'paper':
+            return 'You lose! Sorry bro!'
 
-            elif players_choice == 'rock':
-                if computer_choice == 'scissors':
-                    print('You won! Congrats')
-                elif computer_choice == 'paper':
-                    print('You lose! Sorry bro!')
+    elif players_choice == 'scissors':
+        if computers_choice == 'paper':
+            return 'You won! Congrats'
+        elif computers_choice == 'rock':
+            return 'Oops! You lose! Well, don\'t be discouraged!'
 
-            elif players_choice == 'scissors':
-                if computer_choice == 'paper':
-                    print('You won! Congrats')
-                elif computer_choice == 'rock':
-                    print('Oops! You lose! Well, don\'t be discouraged!')
+def repeat_or_end():
+    while True:
+        inp = input('Do you want to play again? ').lower()
+        if inp in 'yes':
+            return 'yes'
+        else:
+            exit('See ya!')
 
-            ask = input('Would you like to play again? ').lower()
-            if ask == 'yes':
-                continue
-            else:
-                print('See ya!')
-                break
+def main():
+    print('ROCK, PAPER & SCISSORS')
+    while True:
+        players_selection, computers_selection = choices_of_players()
+        players_selection, computers_selection = dead_heat(players_selection, computers_selection)
+        result = game_progress(players_selection, computers_selection)
+        print(result)
+        repeat_or_end()
 
-
-#ВАРИАНТ КОНЦОВКИ С ЦИКЛОМ1
-# ask = input('Would you like to play again? ').lower()
-# while (ask != 'yes') and (ask != 'no'):
-#     print('Wrong value')
-#     ask = input('Would you like to play again? ').lower()
-# if ask == 'yes':
-#     continue
-# elif ask == 'no':
-#     print('See ya!')
-#     break
-
-#ВАРИАНТ КОНЦОВКИ С ЦИКЛОМ2
-# is_exit = False
-# while True:
-#     inp = input('Do you want to play again?').lower()
-#     if inp in ['yes', 'no']:
-#         is_exit = inp == 'no'
-#         break
-# if is_exit:
-#     break
+if __name__ == '__main__':
+    main()
--- homework7/rock_paper_scissors_functions.py
+++ homework7/rock_paper_scissors_functions.py
@@ -21,64 +21,51 @@ def dead_heat(players_choice, computers_choice):
         else:
             return players_choice, computers_choice
 
-def game_progress(players_choice, computers_choice):
+def is_won(players_choice, computers_choice):
     if players_choice == 'paper':
         if computers_choice == 'rock':
-            return 'You won! Congrats'
+            return True
         elif computers_choice == 'scissors':
-            return 'You lose! But that\'s okay!'
+            return False
 
     elif players_choice == 'rock':
         if computers_choice == 'scissors':
-            return 'You won! Congrats'
+            return True
         elif computers_choice == 'paper':
-            return 'You lose! Sorry bro!'
+            return False
 
     elif players_choice == 'scissors':
         if computers_choice == 'paper':
-            return 'You won! Congrats'
+            return True
         elif computers_choice == 'rock':
-            return 'Oops! You lose! Well, don\'t be discouraged!'
+            return False
 
-def repeat_or_end():
+def is_repeat():
     while True:
-        inp = input('Do you want to play again?').lower()
-        if inp in 'yes':
-            return 'yes'
+        inp = input('Do you want to play again? ').lower()
+        if inp == 'yes':
+            return True
         else:
-            print('See ya!')
-        exit()
+            return False
 
 def main():
     print('ROCK, PAPER & SCISSORS')
     while True:
         players_selection, computers_selection = choices_of_players()
         players_selection, computers_selection = dead_heat(players_selection, computers_selection)
-        result = game_progress(players_selection, computers_selection)
-        print(result)
-        repeat_or_end()
+        result = is_won(players_selection, computers_selection)
+        if result is True:
+            print('You won! Congrats')
+        else:
+            print('Oops! You lose! Well, don\'t be discouraged!')
+        agree = is_repeat()
+        if agree is True:
+            continue
+        else:
+            exit('See ya!')
 
 if __name__ == '__main__':
     main()
-
-#ВАРИАНТ КОНЦОВКИ С ЦИКЛОМ1
-# ask = input('Would you like to play again? ').lower()
-# while (ask != 'yes') and (ask != 'no'):
-#     print('Wrong value')
-#     ask = input('Would you like to play again? ').lower()
-# if ask == 'yes':
-#     continue
-# elif ask == 'no':
-#     print('See ya!')
-#     break
-
-#ВАРИАНТ КОНЦОВКИ С ЦИКЛОМ2
-# is_exit = False
-# while True:
-#     inp = input('Do you want to play again?').lower()
-#     if inp in ['yes', 'no']:
-#         is_exit = inp == 'no'
-#         break
 
 # if is_exit:
 #     break
