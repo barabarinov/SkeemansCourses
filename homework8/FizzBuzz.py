0c839b1a1ffcff4c1dba6a44494fe237f58bc101
aleksandrbarinov <barabarinov@gmail.com>
2021-10-10 22:53:41 +0300

    release v1

---
+++ homework8/FizzBuzz.py
@@ -0,0 +1,31 @@
+def fizz_buzz(amount):
+    my_list = []
+    for i in range(amount + 1):
+        if i == 0:
+            continue
+        elif i%3 == 0 and i%5 == 0:
+            my_list.append('fizz buzz')
+        elif i%3 == 0:
+            my_list.append('Fizz')
+        elif i%5 == 0:
+            my_list.append('Buzz')
+        else:
+            my_list.append(f'{i}')
+    return my_list
+print(fizz_buzz(45))
+
+def fizz_buzz_gen(amount):
+    for i in range(amount + 1):
+        if i == 0:
+            continue
+        elif i%3 == 0 and i%5 == 0:
+            yield 'fizz buzz'
+        elif i%3 == 0:
+            yield 'Fizz'
+        elif i%5 == 0:
+            yield 'Buzz'
+        else:
+            yield f'{i}'
+
+for i in fizz_buzz_gen(45):
+    print(f'{i}', end=', ')
