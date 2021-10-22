e90fcbbfc3eda57be2ee8dcbe041d8f8d51ff1c2
aleksandrbarinov <barabarinov@gmail.com>
2021-10-22 11:00:14 +0300

    release v2

--- homework10/weather.py
+++ homework10/weather.py
@@ -2,33 +2,39 @@ import requests
 from pprint import pprint
 from gitignore import TOKEN
 
-api_key = TOKEN
+language = 'lang=uk'
+air_quality = 'aqi=yes'
 city = 'Kiev'
 
-def get_current_weather(api_key, city):
-    return  requests.get(f'http://api.weatherapi.com/v1/current.json?'
-                         f'key={api_key}&lang=uk&aqi=yes&alerts=yes&q={city}').json()
+parameters = {
+    TOKEN: TOKEN,
+    'language': language,
+    'air_quality': air_quality,
+    'city': city,
+}
 
-def get_forecast_weather(api_key, city):
-    return requests.get(f'http://api.weatherapi.com/v1/forecast.json?'
-                        f'key={api_key}&q={city}&days=2&aqi=yes&alerts=yes&lang=uk').json()
+def get_current_weather():
+    return  requests.get(
+        'http://api.weatherapi.com/v1/current.json?',
+        params=parameters
+        )
 
-def get_choice():
-    while True:
-        try:
-            question = int(input("Input '1' to see current weather or '2' to see forecast weather for 2 days: "))
-            if question == 1 or question == 2:
-                return question
-        except ValueError as e:
-            print(f'Error{e}')
+def get_forecast_weather():
+    return requests.get(
+        'http://api.weatherapi.com/v1/forecast.json?',
+        params=parameters
+        )
 
 def main():
-        if get_choice() == 1:
-            print(f'Current weather in {city} is:')
-            pprint(get_current_weather(api_key, city))
-        else:
-            print(f'Weather forecast for two days in {city} is:')
-            pprint(get_forecast_weather(api_key, city))
+    print(f'Current weather in {city} is:')
+    pprint(get_current_weather())
+
+    print(f'Weather forecast for two days in {city} is:')
+    pprint(get_forecast_weather())
 
 if __name__ == '__main__':
     main()
+
+# f'http://api.weatherapi.com/v1/current.json?key={TOKEN}&lang=uk&aqi=yes&alerts=yes&q={city}').json()
+
+#f'http://api.weatherapi.com/v1/forecast.json?key={TOKEN}&q={city}&days=2&aqi=yes&alerts=yes&lang=uk').json()
