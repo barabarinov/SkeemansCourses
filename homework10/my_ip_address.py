969b48a0eacc93719b3e25fe3ae44136b6a647ef
aleksandrbarinov <barabarinov@gmail.com>
2021-10-22 15:34:18 +0300

    release v3

--- homework10/my_ip_address.py
+++ homework10/my_ip_address.py
@@ -1,8 +1,7 @@
 import requests
-import re
 
 some_address = requests.get('https://httpbin.org/ip').text
-print(some_address) #"origin": "185.244.158.114"
+print(some_address)
 
 # pattern = r'\d{2,}\.\d{2,}\.\d{2,}\.\d{2,}.'
 # ip = re.search(pattern, some_address).group()
