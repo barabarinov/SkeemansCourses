411fcd8fa2872f8c7a61dfacccd279983f965d3c
aleksandrbarinov <barabarinov@gmail.com>
2021-10-26 16:21:17 +0300

    release v2

--- homework11/pet_shop/pet_shop.py
+++ homework11/pet_shop/pet_shop.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask, request, Response
+from flask_api import status
 
 
 app = Flask(__name__)
@@ -24,27 +25,29 @@ def get_items():
     )
 
 
-@app.route('/items/', methods=['POST'])
-def give_the_new_pet():
-    import ipdb;
-    ipdb.set_trace()
+@app.route('/items/<int:item_id>/', methods=['POST'])
+def create_item(item_id):
+    # import ipdb;
+    # ipdb.set_trace()
     data = get_data()
+    # print(request.get_json())
     latest_id = max([msg['id'] for msg in data], default=0) + 1
     data.append({
-        latest_id,
-        request.get_json()['message']
+        'id': latest_id,
+        'message': request.get_json()['message'],
+        'amount': request.get_json()['amount'],
     })
     store_data(data)
     return Response(
         json.dumps({
-            'pet_id': latest_id,
+            'item_id': latest_id,
         }),
         mimetype='application/json',
     )
 
 
-@app.route('/pet_shop/<int:pet_id>/', methods=['GET'])
-def get_a_pet(pet_id):
+@app.route('/pet_shop/<int:item_id>/', methods=['GET'])
+def get_item(pet_id):
     data = get_data()
     for i in range(len(data)):
         if data[i]['id'] == pet_id:
@@ -52,19 +55,23 @@ def get_a_pet(pet_id):
                 json.dumps(data[i]),
                 mimetype='application/json',
             )
+        break
+    else:
+        return Response('The pet not found', status=404)
 
 
-@app.route('/pet_shop/<int:pet_id>/', methods=['PUT'])
-def update_a_pet(pet_id):
+@app.route('/pet_shop/<int:item_id>/', methods=['PUT'])
+def update_item(item_id):
     data = get_data()
     for i in range(len(data)):
         pet_shop = data[i]
-        if data[i]['id'] == pet_id:
+        if data[i]['id'] == item_id:
             data[i]['message'] = request.get_json()['message']
+            data[i]['amount'] = request.get_json()['amount']
             store_data(data)
             break
     else:
-        return Response('The pet not found', status=404)
+        return Response('The item not found', status=404)
     return Response(
         json.dumps(pet_shop),
         status=200,
@@ -72,15 +79,15 @@ def update_a_pet(pet_id):
     )
 
 
-@app.route('/pet_shop/<int:pet_id>/', methods=['DELETE'])
-def remove_pet(pet_id):
+@app.route('/pet_shop/<int:item_id>/', methods=['DELETE'])
+def remove_item(item_id):
     data = get_data()
     for i in range(len(data)):
-        if data[i]['id'] == pet_id:
+        if data[i]['id'] == item_id:
             del data[i]
             store_data(data)
             return Response(status=204)
-    return Response('The pet not found', status=404)
+    return Response('The item not found!', status=404)
 
 
 # if __name__ == '__main__':
