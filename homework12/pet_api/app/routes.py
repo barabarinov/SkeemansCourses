4244ace7c6bba308d325dac6b882c90686d9581b
aleksandrbarinov <barabarinov@gmail.com>
2021-11-02 13:46:45 +0200

    release v6

--- homework12/pet_api/app/routes.py
+++ homework12/pet_api/app/routes.py
@@ -1,5 +1,6 @@
 from flask import request
 from flask_api import status
+from flask_login import login_required
 
 
 from app import app
@@ -7,33 +8,44 @@ from app.storage import get_data, store_data
 from app.utils import get_item_index_or_404, item_to_json, get_user_input_json
 
 
-@app.route('/items/', methods=['GET', 'POST'])
+@app.route('/items/', methods=['GET'])
 def all_items_requests_handler():
     data = get_data()
-    if request.method == 'GET':
-        return [item_to_json(item) for item in data]
-    else:
-        user_data = get_user_input_json(request.get_data())
-        item_request = {
-            'id': max([msg['id'] for msg in data], default=0) + 1,
-            'title': user_data['title'],
-            'amount': user_data['amount'],
-            'price': user_data['price'],
-        }
-        data.append(item_request)
-        store_data(data)
-        return item_to_json(item_request), status.HTTP_201_CREATED
+    return [item_to_json(item) for item in data]
 
 
-#Endpoints
-@app.route('/items/<int:item_id>/', methods=['GET', 'PUT', 'DELETE'])
+@app.route('/items/<int:item_id>/', methods=['GET'])
 def single_item_request_handler(item_id):
     data = get_data()
     item_index = get_item_index_or_404(data, item_id)
     item_request = data[item_index]
-    if request.method == 'GET':
-        return item_to_json(item_request)
-    elif request.method == 'PUT':
+    return item_to_json(item_request)
+
+
+@app.route('/items/', methods=['POST'])
+@login_required
+def all_items_requests_handler():
+    data = get_data()
+    user_data = get_user_input_json(request.get_data())
+    item_request = {
+        'id': max([msg['id'] for msg in data], default=0) + 1,
+        'title': user_data['title'],
+        'amount': user_data['amount'],
+        'price': user_data['price'],
+    }
+    data.append(item_request)
+    store_data(data)
+    return item_to_json(item_request), status.HTTP_201_CREATED
+
+
+#Endpoints
+@app.route('/items/<int:item_id>/', methods=['PUT', 'DELETE'])
+@login_required
+def single_item_request_handler(item_id):
+    data = get_data()
+    item_index = get_item_index_or_404(data, item_id)
+    item_request = data[item_index]
+    if request.method == 'PUT':
         user_data = get_user_input_json(request.get_data())
         item_request['title'] = user_data['title']
         item_request['amount'] = user_data['amount']
@@ -58,6 +70,7 @@ def buy_one_item(item_id):
 
 
 @app.route('/items/calculate/', methods=['GET'])
+@login_required
 def sum_of_prices():
     data = get_data()
     sum_of_prices = 0
