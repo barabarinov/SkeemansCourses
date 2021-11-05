b82c7c1e6fd33aefb9b387c2fa16e6317b9d46c2
aleksandrbarinov <barabarinov@gmail.com>
2021-11-05 19:12:53 +0200

    release v3

--- homework14/phone_number.py
+++ homework14/phone_number.py
@@ -1,12 +1,20 @@
 def create_phone_number(nums):
-    phone_number = ''
-    phone_number = ''.join(str(i) for i in (nums[:3]))
-    phone_number = ''.join('(' + phone_number + ') ')
-    phone_number += ''.join(str(j) for j in (nums[3:6]))
-    phone_number = ''.join(phone_number + '-')
-    phone_number += ''.join(str(k) for k in (nums[6:10]))
-    return phone_number
+    nums = list(map(str, nums))
+    return f'({"".join(nums[:3])}) {"".join(nums[3:6])}-{"".join(nums[6:10])}'
 
 
 n = ([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
 print(create_phone_number(n))
+
+
+def create_number(nums):
+    nums = list(map(str, nums))
+    phone_number = ''.join(i for i in (nums[:3]))
+    phone_number = ''.join('(' + phone_number + ') ')
+    phone_number += ''.join(j for j in (nums[3:6]))
+    phone_number = ''.join(phone_number + '-')
+    return phone_number + ''.join(k for k in (nums[6:10]))
+
+
+n = ([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+print(create_number(n))
