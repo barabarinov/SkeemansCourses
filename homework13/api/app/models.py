134058bc7eb647162ea92c22f6be5c4d825bd330
aleksandrbarinov <barabarinov@gmail.com>
2021-11-16 01:30:02 +0200

    bug fixed in hm13

--- homework13/api/app/models.py
+++ homework13/api/app/models.py
@@ -6,20 +6,20 @@ class User(UserMixin):
     def __init__(self, id, username, first_name, last_name, age, password):
         self.id = id
         self.username = username
+        self.password = password
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        self.password = password
 
     def to_json(self):
         return {
             'id': self.id,
             'username': self.username,
+            'password': self.password,
             'first_name': self.first_name,
             'last_name': self.last_name,
             'age': self.age,
-            'password': self.password,
         }
 
     def __repr__(self):
-        return f'User {self.first_name} {self.last_name}'
+        return f'User {self.id} {self.first_name} {self.last_name}'
