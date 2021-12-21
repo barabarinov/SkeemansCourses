fc554abdd27d5b30c67379ced2956e77057c3c20
aleksandrbarinov <barabarinov@gmail.com>
2021-12-21 15:38:30 +0200

    added some kata

--- homework21/double_every_other.py
+++ homework21/double_every_other.py
@@ -1,9 +1,5 @@
 def double_every_other(lst):
     return [value * 2 if i%2 != 0 else value for i, value in enumerate(lst)]
-    # for i, value in enumerate(lst):
-    #     if value % 2 == 0:
-    #         lst[i] *= 2
-    # return lst
 
 
 print(double_every_other([2,3,5,22,211]))
