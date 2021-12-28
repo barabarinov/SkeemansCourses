ec2ab564edf9053728ccc8bdc32364220be1ce75
aleksandrbarinov <barabarinov@gmail.com>
2021-12-28 17:51:36 +0200

    fixed kata 'missing_no'

--- homework21/missing_no.py
+++ homework21/missing_no.py
@@ -2,14 +2,6 @@ import random
 
 
 def missing_no(nums):
-    # counter = 0
-    # while True:
-    #     if counter in nums:
-    #         counter +=1
-    #         continue
-    #     else:
-    #         return counter
-
     return (set(range(0, 101)) - set(nums)).pop()
 
 
