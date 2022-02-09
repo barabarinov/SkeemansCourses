80256cd2eca8881a50ebe1361b379db3e15b640e
aleksandrbarinov <barabarinov@gmail.com>
2022-02-09 18:19:49 +0200

    added ternary operator version

--- homework24/get_middle.py
+++ homework24/get_middle.py
@@ -1,10 +1,22 @@
 def get_middle(s):
     if not s:
         return ''
+    return s[(len(s) // 2) - 1: len(s) // 2 + 1] if len(s)%2 == 0 else s[len(s) // 2]
+
+
+print(get_middle('n'))
+
+
+def get_middle_2(s):
+    if not s:
+        return ''
     if len(s)%2 == 0:
+        print((len(s) // 2) - 1)
+        print(len(s) // 2 + 1)
         return s[(len(s) // 2) - 1: len(s) // 2 + 1]
     else:
+        print(s[0])
         return s[len(s) // 2]
 
 
-print(get_middle('karkarkarr'))
+print(get_middle_2('nm'))
