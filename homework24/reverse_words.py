6da7c8af99bccb846204e38b7b1e0ad04ce19c09
aleksandrbarinov <barabarinov@gmail.com>
2022-02-25 13:52:48 +0200

    added two variations of kata

--- homework24/reverse_words.py
+++ homework24/reverse_words.py
@@ -1,6 +1,23 @@
 def reverse_words(text):
-    print(list(text))
-    return ''.join(word[::-1] for word in list(text))
+    out = ''
+    total = ''
+    for letter in list(text):
+        if letter not in ' ':
+            out += letter
+        else:
+            total += ''.join(out[::-1])
+            out = ''
+            total += ''.join(letter)
+    total += ''.join(out[::-1])
+    return total
 
 
-print(reverse_words('Albina Bethel Circa Den Enemy'))
+print(reverse_words('double  spaced  words'))
+
+
+def reverse_words_two(text):
+    return ' '.join(s[::-1] for s in text.split(' '))
+
+
+print(reverse_words_two('a b   c d'))
+# PRAY FOR UKRAINE
