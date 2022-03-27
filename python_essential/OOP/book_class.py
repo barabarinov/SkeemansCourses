2d7676c6a99795e0a763c8a412aa3e22453e1c57
aleksandrbarinov <barabarinov@gmail.com>
2022-03-27 23:19:51 +0300

    fixed book_info

--- python_essential/OOP/book_class.py
+++ python_essential/OOP/book_class.py
@@ -18,10 +18,10 @@ class Book:
         self.review_list = []
 
     def __repr__(self):
-        return f'Book: {self.name}, Author: {self.author}, Published date: {self.published_year.isoformat()}, Genre: {self.genre.value}'
+        return f'Book: {self.name}, Author: {self.author}, Published date: {self.published_year.isoformat()}, Genre: {self.genre.value}\n{"No reviews" if not self.review_list else str(self.review_list)}'
 
     def __str__(self):
-        return f'Book: {self.name}, Author: {self.author}, Published date: {self.published_year.isoformat()}, Genre: {self.genre.value}'
+        return f'Book: {self.name}, Author: {self.author}, Published date: {self.published_year.isoformat()}, Genre: {self.genre.value}\n{"No reviews" if not self.review_list else str(self.review_list)}'
 
     def __eq__(self, other):
         return self.name == other.name and self.author == other.author and self.published_year == other.published_year and self.genre == other.genre
@@ -31,17 +31,23 @@ class Book:
 
     def book_info(self):
         for review in self.review_list:
-            print(f'Review of "{self.name}" by: {review.user}. Text: {review.text} Time of Creation: {review.time}')
+            if not self.review_list:
+                return f'Book "{self.name}" has no reviews!'
+            else:
+                return f'Review of "{self.name}" by: {review.user}. Text: {review.text} Time of Creation: {review.time.strftime("%H:%M  %d/%m/%Y")}'
 
 
 class Review:
     def __init__(self, user: str, text: str, time: datetime):
         self.user = user
         self.text = text
-        self.time = time # datetime.datetime.strftime('%H:%M  %d/%m/%Y')
+        self.time = time
 
     def __str__(self):
-        return f'Reviev by: {self.user}. Text: {self.text} time of creation: {self.time}'
+        return f'Review by: {self.user}. Text: {self.text} Time of Creation: {self.time.strftime("%H:%M  %d/%m/%Y")}'
+
+    def __repr__(self):
+        return f'Review by: {self.user}. Text: {self.text} Time of Creation: {self.time.strftime("%H:%M  %d/%m/%Y")}'
 
 
 def main():
@@ -52,12 +58,11 @@ def main():
     book1.add_review('barinovman', 'This book is super great! I like it!', datetime.datetime.now())
     book3.add_review('VladLyt', 'My favorite poetry book! Love it! 🇺🇦', datetime.datetime.now())
 
-    print(book1)
-    print(book2)
-    print(book3)
-    print(book4)
-    print(book1.book_info())
-
+    # print(book1)
+    # print(book2)
+    # print(book3)
+    print(book4.book_info())
+    # print(book4.review_list)
 
 if __name__ == '__main__':
     main()
