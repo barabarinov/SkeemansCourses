6314d9c93c63b128c0068f86870c7f99001b6032
aleksandrbarinov <barabarinov@gmail.com>
2022-04-01 21:39:45 +0300

    fixed calculate

--- python_essential/02_exeptions/calculate.py
+++ python_essential/02_exeptions/calculate.py
@@ -2,23 +2,37 @@
 # сложение, вычитание, умножение, деление и возведение в степень.
 # Программа должна выдавать сообщения об ошибке и продолжать работу
 # при вводе некорректных данных, делении на ноль и возведении нуля в отрицательную степень.
-from operator import sub, add, mul, truediv, pow
+from operator import sub, add, mul
 
 
-def calc(a, oparation, b):
+def powpow(a, b):
+    try:
+        return a ** b
+    except ZeroDivisionError as e:
+        print('Error: ', e)
+
+
+def divdiv(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError as e:
+        print('Error: ', e)
+
+
+def calculate(a, oparation, b):
     mapping = {
         '-': sub,
         '+': add,
         '*': mul,
-        '/': truediv,
-        '**': pow,
+        '/': divdiv,
+        '**': powpow,
     }
     if oparation not in mapping:
         return 'Sign is not correct!'
     try:
         return mapping[oparation](a, b)
-    except (ZeroDivisionError, TypeError) as e:
-        print('Error', e)
+    except TypeError as e:
+        return 'Error: ', e
 
 
-print(calc(0, '**', 10))
+print(calculate(0, '**', -30))
