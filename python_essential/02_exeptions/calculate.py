9d7e92d58aced431c8d46b5344f844738aff3689
aleksandrbarinov <barabarinov@gmail.com>
2022-04-02 00:35:38 +0300

    finished calculate

--- python_essential/02_exeptions/calculate.py
+++ python_essential/02_exeptions/calculate.py
@@ -1,7 +1,3 @@
-# Напишите программу-калькулятор, которая поддерживает следующие операции:
-# сложение, вычитание, умножение, деление и возведение в степень.
-# Программа должна выдавать сообщения об ошибке и продолжать работу
-# при вводе некорректных данных, делении на ноль и возведении нуля в отрицательную степень.
 from operator import sub, add, mul
 
 
@@ -26,11 +22,9 @@ def calculate():
             break
         else:
             a, operation, b = expression.split()
-            # print(type(a), type(operation), type(b))
             a = int(a)
             operation = str(operation)
             b = int(b)
-            # print(type(a), type(operation), type(b))
             mapping = {
                 '-': sub,
                 '+': add,
@@ -42,10 +36,9 @@ def calculate():
                 print('Sign is not correct!')
                 continue
             try:
-                # print(f'Result: {mapping[operation](a, b) if mapping[operation](a, b) is not None else ""}')
                 print(f'{"Result: " + str(mapping[operation](a, b)) if mapping[operation](a, b) is not None else ""}')
             except TypeError as e:
                 print('Error: ', e)
 
 
-print(calculate())
+calculate()
