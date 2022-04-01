1ce1d286152eec2a2011c6ab27fb3a9a92c4c522
aleksandrbarinov <barabarinov@gmail.com>
2022-04-01 12:26:08 +0300

    added exeption

--- python_essential/02_exeptions/exeption.py
+++ python_essential/02_exeptions/exeption.py
@@ -3,25 +3,32 @@ WIDTH = 80
 
 
 def print_result(number):
-    stars_count = round(MAX_STARS / number)
-    if stars_count > MAX_STARS:
+    if number == 0:
         stars_count = MAX_STARS
+    else:
+        stars_count = round(MAX_STARS / number)
+        if stars_count > MAX_STARS:
+            stars_count = MAX_STARS
+
     number_field_width = WIDTH - 2 * stars_count
     stars  = '*' * stars_count
     fmt = '{0}{1:^' + str(number_field_width) + '}{0}'
 
-    print(fmt.format(stars, number))
+    return fmt.format(stars, number)
 
 
-def div(a, b):
+def div():
     while True:
         try:
-            result = a / b
-            print_result(result)
-            break
+            first_number = float(input('Enter first number: '))
+            second_number = float(input('Enter second number: '))
+            result = first_number / second_number
         except (ValueError, ZeroDivisionError) as e:
             print('Error: ', e)
             print('Please try again!')
+        else:
+            print(print_result(result))
+            break
 
 
-div(5, 2)
+div()
