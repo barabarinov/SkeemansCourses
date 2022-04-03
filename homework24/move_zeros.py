31db7eefd0ef829238a154512bb32b4620724a75
aleksandrbarinov <barabarinov@gmail.com>
2022-04-03 19:48:44 +0300

    added custom function MyRange()

--- homework24/move_zeros.py
+++ homework24/move_zeros.py
@@ -9,4 +9,19 @@ def move_zeros(numbers):
     return nums + zeros
 
 
-print(move_zeros([0]))
+print(move_zeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
+
+
+def solve(some_list):
+    k = 0
+    for i in range(len(some_list)):
+        if some_list[i] != 0:
+            some_list[k] = some_list[i]
+            k+=1
+    for j in range(k,len(some_list)):
+        some_list[j] = 0
+    return some_list
+
+
+l = [9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]  # [2, 0, 1, 4, 0, 5, 6, 4, 0, 1, 7]
+print(solve(l))
