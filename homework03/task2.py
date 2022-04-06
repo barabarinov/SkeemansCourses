9a748d05d244b5aa470b169b329e471229915d2e
aleksandrbarinov <barabarinov@gmail.com>
2022-04-06 20:44:00 +0300

    fixed is_prime_num

--- homework03/task2.py
+++ homework03/task2.py
@@ -1,20 +1,20 @@
 def is_prime(number):
     if number < 2:
-        return False
+        return number, False
 
     for i in range(2, number // 2 + 1):
         if number % i == 0:
-            return False
-    return True
+            return number, False
+    return number, True
 
 
 def get_number_and_check_is_prime():
     number = int(input("Choose your number: "))
-
-    if is_prime(number):
-        print('Number is prime')
+    numbers, is_prime_num = is_prime(number)
+    if is_prime_num:
+        print(f'{numbers} is prime number')
     else:
-        print('Number is not prime')
+        print(f'{numbers} is not prime number')
 
 
 number_of_checks = int(input("Choose your number of checks: "))
--- python_essential/03_generators/is_prime_num.py
+++ python_essential/03_generators/is_prime_num.py
@@ -1,31 +1,32 @@
-def is_prime_num(number):
-    if number < 2:
-        yield False
-
-    for j in range(2, number // 2 + 1):
-        if number % j == 0:
-            yield False
-    yield True
+# def is_prime_num(number):
+#     if number < 2:
+#         return False
+#
+#     for j in range(2, number // 2 + 1):
+#         if number % j == 0:
+#             return False
+#     return True
 
 
 def get_number_and_check_is_prime(number):
     for i in range(1, number + 1):
         if i < 2:
-            yield False
+            yield i, False
+            continue
         for j in range(2, i // 2 + 1):
             if i%j == 0:
                 yield i, False
-                break
         yield i, True
 
 
-number = int(input("Choose your number: "))
-for num in get_number_and_check_is_prime(number):
-    print(num)
-    if num:
-        print(f'Number is prime')
+# number = int(input("Choose your number: "))
+number = 5
+for num, is_prime in get_number_and_check_is_prime(number):
+    if is_prime:
+        print(f'{num} is prime number')
     else:
-        print(f'Number is not prime')
+        print(f'{num} is not prime number')
+
 # 1
 # 2 Prime
 # 3 Prime
