9eeb7fd53ac2f47f61f0d764b716b57eb778ff51
aleksandrbarinov <barabarinov@gmail.com>
2022-04-06 21:05:43 +0300

    fixed is_prime_num

--- python_essential/03_generators/is_prime_num.py
+++ python_essential/03_generators/is_prime_num.py
@@ -1,11 +1,10 @@
-# def is_prime_num(number):
-#     if number < 2:
-#         return False
-#
-#     for j in range(2, number // 2 + 1):
-#         if number % j == 0:
-#             return False
-#     return True
+def is_prime_num(number):
+    if number < 2:
+        return False
+    for j in range(2, number // 2 + 1):
+        if number % j == 0:
+            return False
+    return True
 
 
 def get_number_and_check_is_prime(number):
@@ -13,24 +12,12 @@ def get_number_and_check_is_prime(number):
         if i < 2:
             yield i, False
             continue
-        for j in range(2, i // 2 + 1):
-            if i%j == 0:
-                yield i, False
-                break
-        else:
-            yield i, True
+        yield i, is_prime_num(i)
 
 
-# number = int(input("Choose your number: "))
 number = 5
 for num, is_prime in get_number_and_check_is_prime(number):
     if is_prime:
         print(f'{num} is prime number')
     else:
         print(f'{num} is not prime number')
-
-# 1
-# 2 Prime
-# 3 Prime
-# 4 Not Prime
-# 5 Prime
