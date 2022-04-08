6e2a116617085fd3919b47306068f337b9d3dda3
aleksandrbarinov <barabarinov@gmail.com>
2022-04-08 21:21:40 +0300

    fixeded kata

--- homework24/count_words_generator.py
+++ homework24/count_words_generator.py
@@ -3,16 +3,14 @@ from collections import defaultdict
 
 
 def count_words_generator(file_lines):
-    counted_chars = {}
+    counted_chars = defaultdict(int)
     for line in file_lines:
-        for key, value in Counter(filter(lambda x: x.isalpha(), line.lower())):
-            if key not in counted_chars:
-                counted_chars[key] = 0
+        for key, value in Counter(filter(lambda x: x.isalpha(), line.lower())).items():
             counted_chars[key] += value
         yield dict(counted_chars)
 
 
 if __name__ == '__main__':
-    with open('/text.txt') as f:
+    with open('text.txt') as f:
         for counted_words in count_words_generator(f):
             print(counted_words)
