cb3fe3d2ba946fa9354d32fcaae23a133b130265
aleksandrbarinov <barabarinov@gmail.com>
2022-04-16 20:15:52 +0300

    added kata

---
+++ homework25/same_from_two_str.py
@@ -0,0 +1,10 @@
+def same_letters(string1, string2):
+    return set(string1) & set(string2)
+
+# & Вертає символи, що є в обох сетах.
+# | Вертає усі унікальні символи з двох сетів.
+# ^ Вертає різницю двох сетів. Тобто унікальні символи кожного сета.
+# - Вертає усі символи які є у string1 і немає у string2
+
+
+print(same_letters('Programming', 'Drumming'))
