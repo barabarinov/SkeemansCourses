3bdf739c8301cff5e720baf9682191cfaf89e73e
aleksandrbarinov <barabarinov@gmail.com>
2022-04-24 20:28:39 +0300

    fixed kata

--- homework25/rot13decoder.py
+++ homework25/rot13decoder.py
@@ -1,18 +1,6 @@
 from termcolor import colored
 
 
-# def rot13(s):
-#     out = ''
-#     for letter in s:
-#         if not letter.isalpha():
-#             out += letter
-#         elif letter.isupper():
-#             out += chr(65 + ord(letter) % 26)
-#         elif letter.islower():
-#             out += chr(((ord(letter) - 97) + 13) % 26 + 97)
-#     return out
-
-
 def rot13(s):
     return "".join([(chr(((ord(c) - 97) + 13) % 26 + 97) if c.islower() else chr(
         ((ord(c) - 65) + 13) % 26 + 65)) if c.isalpha() else c for c in s])
